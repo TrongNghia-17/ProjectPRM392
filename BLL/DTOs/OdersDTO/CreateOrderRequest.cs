6a6020d6ac32672377ee1cd8cbfbf04a8231b46c
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-07-10 21:35:48 +0700

    xóa status trong dto orderitem

--- BLL/DTOs/OdersDTO/CreateOrderRequest.cs
+++ BLL/DTOs/OdersDTO/CreateOrderRequest.cs
@@ -18,7 +18,6 @@ namespace BLL.DTOs.OdersDTO
         public Guid ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
-        public string Status { get; set; }
         public decimal Price { get; set; }
     }
     public class OrderResponseDto
