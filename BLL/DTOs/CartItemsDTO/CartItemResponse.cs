b1b1ab91c62b3a85571ed936484b8d6a37874d7f
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-07-21 13:27:17 +0700

    fix cart có thêm img

--- BLL/DTOs/CartItemsDTO/CartItemResponse.cs
+++ BLL/DTOs/CartItemsDTO/CartItemResponse.cs
@@ -5,6 +5,7 @@ public class CartItemResponse
     public Guid CartItemId { get; set; }
     public Guid ProductId { get; set; }
     public string ProductName { get; set; } = null!;
+    public string ImageUrl { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
     public DateTime AddedAt { get; set; }
